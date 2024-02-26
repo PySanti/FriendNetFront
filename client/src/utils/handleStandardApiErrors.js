f58e10814b1d5c92f5dd14c50cd9ddca9ee2b5de
santiago <santiagodeandrade4@gmail.com>
2024-02-26 08:46:56 -0400

    Modificacion para mejora en el manejo de errores

--- client/src/utils/handleStandardApiErrors.js
+++ client/src/utils/handleStandardApiErrors.js
@@ -6,10 +6,16 @@ import {toast} from "sonner"
  * Retornara true en caso de que se logre manejar el error, false en caso contrario
  */
 export function handleStandardApiErrors(error){
-    if (error.message === BASE_FALLEN_SERVER_ERROR_MSG  || error.response.status == 403){
-        toast.error(error.message === BASE_FALLEN_SERVER_ERROR_MSG ? BASE_FALLEN_SERVER_LOG : BASE_RATE_LIMIT_BLOCK_RESPONSE)
+    try {
+        if (error.message === BASE_FALLEN_SERVER_ERROR_MSG  || error.response.status == 403){
+            toast.error(error.message === BASE_FALLEN_SERVER_ERROR_MSG ? BASE_FALLEN_SERVER_LOG : BASE_RATE_LIMIT_BLOCK_RESPONSE)
+            return true;
+        } else {
+            return false;
+        }
+    } catch{
+        toast.error("¡ Error inesperado !")
         return true;
-    } else {
-        return false;
     }
+
 }
