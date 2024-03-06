bf5dfc0237b665867d82e3b0b390739452a85562
santiago <santiagodeandrade4@gmail.com>
2024-03-05 21:32:10 -0400

    Modificacion para debug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -61,7 +61,7 @@ function App() {
     if (NotificationsWSCanBeUpdated()){
       NOTIFICATIONS_WEBSOCKET.current.onmessage = (event)=>{
         if (event.data == "pong"){
-          console.log("pong")
+          window.alert("recibiendo pong")
           const currentLastPong = new Date()
           lastPong.current = currentLastPong
           setTimeout(() => {
