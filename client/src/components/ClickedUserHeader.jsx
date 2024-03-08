d6a23c17924daad2859ff7193488204b9e8bf68a
santiago <santiagodeandrade4@gmail.com>
2024-03-08 08:05:54 -0400

    Implementacion de transiciones para user_is_online y user_is_typing

--- client/src/components/ClickedUserHeader.jsx
+++ client/src/components/ClickedUserHeader.jsx
@@ -10,7 +10,7 @@ export function ClickedUserHeader(){
     let clickedUser = useClickedUser((state)=>(state.clickedUser))
     const typingDB = useTypingDB((state)=>(state.typingDB))
     return (
-        <div className="clicked-user-header-container">
+        <div className= "clicked-user-header-container">
             {
                 executingInSmallDevice &&
                 <span className="material-symbols-outlined back-chat-button" onClick={()=>resetChats()}>
@@ -19,8 +19,12 @@ export function ClickedUserHeader(){
             }
             <UserPhoto photoFile={clickedUser.photo_link} chatPhoto/>
             <div className="clicked-user__username-container">
-                <h3 className="clicked-user__username">{clickedUser.username}{clickedUser.is_online && ", en linea"}</h3>
-                <h3 className="clicked-user__typing">{typingDB[clickedUser.id] && "escribiendo ... "}</h3>
+                <h3 className="clicked-user__username">
+                    {clickedUser.username}<span className={clickedUser.is_online ? "clicked-user-online clicked-user-online__activated" : "clicked-user-online"}> - en linea</span>
+                </h3>
+                <h3 className={typingDB[clickedUser.id] ? "clicked-user__typing clicked-user__typing__activated" : "clicked-user__typing"}>
+                    escribiendo ...
+                </h3>
             </div>
         </div>
     )
