984f2b1cd9fc865ccd662cee28e652609f40f068
santiago <santiagodeandrade4@gmail.com>
2024-03-13 09:41:27 -0400

    Eliminacion de loading toast en el nonToastedApiCall a traves de una lista de toast

--- client/src/utils/nonToastedApiCall.js
+++ client/src/utils/nonToastedApiCall.js
@@ -3,17 +3,19 @@ import {executeApi} from "./executeApi"
 
 // funcion creada como wrapper para llamadas a apis en las que se quiera renderizar un toast.loading
 // despues de x cantidad de tiempo (timer)
+
+const toastIdsList = []
 export async function  nonToastedApiCall(apiCalling, navigateFunc, loadingMsg, timer){
     let resolved = false;
-    let toastId = false;
     setTimeout(() => {
         if (!resolved){
-            toastId = toast.loading(loadingMsg)
+            toastIdsList.push(toast.loading(loadingMsg))
         }
     }, timer);
     const response = await executeApi(apiCalling, navigateFunc)
     resolved = true
-    if (toastId)
-        toast.dismiss(toastId)
+    toastIdsList.forEach(element => {
+        toast.dismiss(element)
+    });
     return response
 }
