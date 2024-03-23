a37064d0bc0ecc0c051994d85ba57499ae75e184
santiago <santiagodeandrade4@gmail.com>
2024-03-22 22:05:55 -0400

    Solcion a error de no mantenimiento de value del userFilter

--- client/src/components/UserFilter.jsx
+++ client/src/components/UserFilter.jsx
@@ -1,20 +1,27 @@
 import {BASE_USERNAME_MAX_LENGTH} from "../utils/constants"
 import "../styles/UserFilter.css"
 import {useUserKeyword} from "../store"
+import {useRef, useEffect} from "react"
 
 
 /**
  * Filtro de busqueda de usuarios
  */
 export function UserFilter(){
-    let setUserKeyword = useUserKeyword((state)=>(state.setUserKeyword))
-
+    let [userKeyword, setUserKeyword] = useUserKeyword((state)=>([state.userKeyword, state.setUserKeyword]))
+    const userFilterInputRef = useRef(null)
+    useEffect(()=>{
+        if (userFilterInputRef.current && userKeyword && userKeyword.length > 0){
+            userFilterInputRef.current.value = userKeyword
+        }
+    }, [])
     return (
         <div className="user-filter-container">
             <span className="material-symbols-outlined loupe">
                 search
             </span>
             <input 
+                ref={userFilterInputRef}
                 placeholder="Buscar" 
                 className="users-filter-input non-shadow-input" 
                 type="text" 
