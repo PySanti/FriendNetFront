4c99802c658c78c080dbe3c6bc570e07f438fb8e
santiago <santiagodeandrade4@gmail.com>
2024-04-08 17:34:05 -0400

    Eliminacion de seteo de connectionFailed desde connectionFailedPage: no es necesario

--- client/src/pages/ConnectionFailedPage.jsx
+++ client/src/pages/ConnectionFailedPage.jsx
@@ -1,12 +1,9 @@
 import {Header} from "../components/Header.jsx"
 import {Button} from "../components/Button.jsx"
 import {logoutUser} from "../utils/logoutUser"
-import {useConnectionFailed} from "../store"
 export function ConnectionFailedPage(){
-    let setConnectionFailed = useConnectionFailed((state)=>(state.setConnectionFailed))
     const handleButtonClick = ()=>{
         logoutUser(undefined)
-        setConnectionFailed(false)
     }
     return (
         <div className="centered-container">
