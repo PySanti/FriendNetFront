5fa8b4293d1e7c39052314b1ee9ad6f356246a28
santiago <santiagodeandrade4@gmail.com>
2024-04-15 14:25:28 -0400

    Modificacion de IconedInput para posibilidad de iconos a la derecha

--- client/src/components/IconedInput.jsx
+++ client/src/components/IconedInput.jsx
@@ -1,11 +1,21 @@
 import "../styles/IconedInput.css"
 import {PropTypes} from "prop-types"
 
-export function IconedInput({children, icon}){
+export function IconedInput({children, icon, right}){
     return (
         <div className="iconed-input-container">
-            {icon}
-            {children}
+            {
+                right ?
+                <>
+                    {children}
+                    {icon}
+                </>
+                :
+                <>
+                    {icon}
+                    {children}
+                </>
+            }
         </div>
     )
 }
@@ -14,5 +24,10 @@ export function IconedInput({children, icon}){
 IconedInput.propTypes = {
     children : PropTypes.object.isRequired,
     icon : PropTypes.object.isRequired,
+    right : PropTypes.bool
+}
+
+IconedInput.defaultProps = {
+    right : undefined,
 }
 
