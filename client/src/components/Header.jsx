c6fdbdeaec5c1145aa9e803a50794b55e58dbb76
santiago <santiagodeandrade4@gmail.com>
2024-04-30 08:05:51 -0400

    Descarga de header-logo para mayor velocidad de carga y no depender del internet

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,22 +9,15 @@ import {useDarkMode} from "../store"
 export function Header({msg}) {
     const userData = getUserDataFromLocalStorage()
     let darkModeActivated  = useDarkMode((state)=>(state.darkModeActivated))
-    const logo1URL = "https://res.cloudinary.com/dshsg4isr/image/upload/h_436,q_auto:best,w_400/v1709227007/logo3V3_odc3mj.png"
-    const logo2URL = "https://res.cloudinary.com/dshsg4isr/image/upload/h_436,q_auto:best,w_400/v1709227006/logo3V3Inverted_ap1vlz.png"
+    const logo1URL = "/header-logo/header-logo-inv.png"
+    const logo2URL = "/header-logo/header-logo.png"
 
     return (
         <>
             <header className="header-container">
                 <div className="header-title-container">
                     <h1 className="header-content">
-                        {
-                            navigator.onLine ?
-                                <img 
-                                    className="header-logo" 
-                                    src={!darkModeActivated ? logo1URL : logo2URL}/>
-                            :
-                                'F'
-                        }
+                        <img className="header-logo" src={!darkModeActivated ? logo2URL : logo1URL}/>
                         riendNet{userData && `, ${userData.username}`}
                     </h1>
                 </div>
