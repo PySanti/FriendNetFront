419caa7ae3883e8812bdda91fa23172c53cd7aed
santiago <santiagodeandrade4@gmail.com>
2024-07-29 10:16:53 -0400

    Implementacion de try-catch para sistema de busqueda de IP publica de los clientes

--- client/src/api/baseConfig.api.js
+++ client/src/api/baseConfig.api.js
@@ -1,11 +1,16 @@
 import axios from 'axios'
+import {toast} from "sonner"
 
 let clientIp = undefined
 
 
 export async function getBasicConfig(){
     if (clientIp == undefined){
-        clientIp  = (await axios.get('https://api64.ipify.org?format=json')).data.ip
+        try{
+            clientIp  = (await axios.get('https://api64.ipify.org?format=json')).data.ip
+        } catch {
+            toast.error("Error buscando IP publica del usuario, inténtalo de nuevo mas tarde")
+        }
     }
     return {
         headers: {
